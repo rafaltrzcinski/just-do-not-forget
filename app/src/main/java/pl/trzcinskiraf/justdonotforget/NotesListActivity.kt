08b0df39ceab7dfb44c37ca9b9d4dd222214c2e7
rt90 <rt90@wp.pl>
2016-03-04 13:52:44 +0100

    activities + layouts

--- app/src/main/java/pl/trzcinskiraf/justdonotforget/NotesListActivity.kt
+++ app/src/main/java/pl/trzcinskiraf/justdonotforget/NotesListActivity.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 
-class MainActivity : AppCompatActivity() {
+class NotesListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.notes_list_layout)
         val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
 
