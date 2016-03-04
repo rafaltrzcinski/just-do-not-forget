5c279f3e9049ca30569e6eadede067d1ecc901b9
rt90 <rt90@wp.pl>
2016-03-04 13:53:21 +0100

    note activity + layout

---
+++ app/src/main/java/pl/trzcinskiraf/justdonotforget/NoteActivity.kt
@@ -0,0 +1,12 @@
+package pl.trzcinskiraf.justdonotforget
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class NoteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.note_layout)
+    }
+}
