5ecfb84e54fe17a6b16af8936492a381b5205765
rt90 <rt90@wp.pl>
2016-03-04 14:00:54 +0100

    add uuid to Note

--- app/src/main/java/pl/trzcinskiraf/justdonotforget/domain/Note.kt
+++ app/src/main/java/pl/trzcinskiraf/justdonotforget/domain/Note.kt
@@ -2,12 +2,15 @@ package pl.trzcinskiraf.justdonotforget.domain
 
 import android.os.Parcel
 import android.os.Parcelable
+import java.util.*
 
 data class Note(
         val title: String,
-        val content: String) : Parcelable {
+        val content: String,
+        val uuid: String = randomUUID()) : Parcelable {
 
     companion object {
+        fun randomUUID() = UUID.randomUUID().toString()
         @JvmField final val CREATOR = object : Parcelable.Creator<Note> {
             override fun createFromParcel(source: Parcel) =
                     Note(title = source.readString(), content = source.readString())
