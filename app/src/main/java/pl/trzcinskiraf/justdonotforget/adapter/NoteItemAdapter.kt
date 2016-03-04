32a52c10636ac32f0176994e01fdf52f3e77cbff
rt90 <rt90@wp.pl>
2016-03-04 22:39:14 +0100

    add green robot EventBus

--- app/src/main/java/pl/trzcinskiraf/justdonotforget/adapter/NoteItemAdapter.kt
+++ app/src/main/java/pl/trzcinskiraf/justdonotforget/adapter/NoteItemAdapter.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import org.greenrobot.eventbus.EventBus
 import pl.trzcinskiraf.justdonotforget.R
 import pl.trzcinskiraf.justdonotforget.domain.Note
+import pl.trzcinskiraf.justdonotforget.event.NoteClickEvent
 
 class NoteItemAdapter(private val note: Note) : ItemAdapter {
 
@@ -22,7 +24,13 @@ class NoteItemAdapter(private val note: Note) : ItemAdapter {
         val noteHolder = holder as NoteItemHolder
         noteHolder.title.text = note.title
         noteHolder.content.text = note.content
+        val view = noteHolder.itemView
+        view.setOnClickListener(onNoteClicked)
+    }
 
+    private val onNoteClicked = { view: View ->
+        val noteClickEvent = NoteClickEvent(note)
+        EventBus.getDefault().post(noteClickEvent)
     }
 
     private inner class NoteItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
---
+++ app/src/main/java/pl/trzcinskiraf/justdonotforget/event/NoteClickEvent.kt
@@ -0,0 +1,6 @@
+package pl.trzcinskiraf.justdonotforget.event
+
+import pl.trzcinskiraf.justdonotforget.domain.Note
+
+class NoteClickEvent(val note: Note) {
+}
