115444c3e2307a7ec5ba59996e1d53c834e41a99
rt90 <rt90@wp.pl>
2016-03-04 15:18:58 +0100

    refactor + notes list layout as recycler view + simple note item design

--- app/src/main/java/pl/trzcinskiraf/justdonotforget/NotesListActivity.kt
+++ app/src/main/java/pl/trzcinskiraf/justdonotforget/NotesListActivity.kt
@@ -2,16 +2,12 @@ package pl.trzcinskiraf.justdonotforget
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
 
 class NotesListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.notes_list_layout)
-        val toolbar = findViewById(R.id.toolbar) as Toolbar
-        setSupportActionBar(toolbar)
-
     }
 
 }
